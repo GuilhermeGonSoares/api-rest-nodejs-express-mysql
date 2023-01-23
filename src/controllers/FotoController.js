878de7e783a88f6ac7318e20cd6a4ceab77a0200
Guilherme Gonçalves <guilherme16.gon@gmail.com>
2023-01-23 09:33:44 -0300

    add foto ao BD

--- src/controllers/FotoController.js
+++ src/controllers/FotoController.js
@@ -1,6 +1,25 @@
+import multer from 'multer';
+import multerConfig from '../config/multerConfig';
+
+import Foto from '../models/Foto';
+
+const upload = multer(multerConfig).single('arquivo');
+
 class FotoController {
   async upload(req, res) {
-    res.status(201).json("Página inicial!");
+    return upload(req, res, async (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+
+      const { originalname, filename } = req.file;
+      const { aluno_id } = req.body;
+      const foto = await Foto.create({ originalname, filename, aluno_id });
+
+      return res.json(foto);
+    });
   }
 }
 
